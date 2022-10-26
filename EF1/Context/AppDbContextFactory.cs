a123d35579398e96b7d3fcced39c90e789c9deaa
mpilarmartinez <mariapilarmartinezjarque@gmail.com>
2022-10-26 23:51:09 +0200

    Código Proyecto

---
+++ EF1/Context/AppDbContextFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFC1.Context
+{
+    internal class AppDbContextFactory
+    {
+    }
+}
---
+++ EF1/GlobalUsing.cs
@@ -0,0 +1,8 @@
+﻿//quitamos el contenido de la clase y añadimos global using
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.ChangeTracking;
+global using Microsoft.EntityFrameworkCore.Design;
+global using Microsoft.EntityFrameworkCore.Metadata;
+
+global using EFC1;
+
